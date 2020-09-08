cb324d14abb2fde18e1b61cf8c044fef64b1da45
nikolina_gavranovic <nikolinagavranovic98@gmail.com>
2020-09-08 18:36:40 +0200

    Projekat dodan. Model podataka napravljen.

---
+++ RES/ModelPodataka/Veza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelPodataka
+{
+    public class Veza
+    {
+        public int IdVeze { get; set; }
+        public int IdPrvogResursa { get; set; }
+        public int IdDrugogResursa { get; set; }
+        public TipVeze Tip  { get; set; }
+
+        public Veza(int idVeze, int idPrvogResursa, int idDrugogResursa, TipVeze tip)
+        {
+            IdVeze = idVeze;
+            IdPrvogResursa = idPrvogResursa;
+            IdDrugogResursa = idDrugogResursa;
+            Tip = tip;
+        }
+
+        public Veza() { }
+    }
+}
