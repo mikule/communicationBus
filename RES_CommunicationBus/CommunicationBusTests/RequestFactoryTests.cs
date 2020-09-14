1e8affc7693a9a9711144828d41b17bbc716dc77
nikolina_gavranovic <nikolinagavranovic98@gmail.com>
2020-09-14 16:39:28 +0200

    Unit tests for RequestFactory and ValidationOfRequest.

---
+++ RES_CommunicationBus/CommunicationBusTests/RequestFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Common.CommunicationModel;
+using Common.Helper;
+using Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommunicationBusTests
+{
+    [TestClass]
+    public class RequestFactoryTests
+    {
+        [TestMethod]
+        public void ConvertStringToRequest_MakingRequest_ReturnNotNullValue()
+        {
+            string stringRequest = "GET/resurs/1";
+
+            Request request = RequestFactory.ConvertStringToRequest(stringRequest);
+
+            Assert.IsNotNull(request);
+
+        }
+
+        [TestMethod]
+        public void ValidateRequest_PassTheCheck_ReturnTrue()
+        {
+            string stringRequest = "POST/resurs/1";
+            var validation = new ValidationOfRequest();
+
+            var result = validation.ValidateRequest(stringRequest);
+
+            Assert.IsTrue(result);
+        }
+
+    }
+}
---
+++ RES_CommunicationBus/CommunicationBusTests/ValidationOfRequestTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Helpers;
+
+namespace CommunicationBusTests
+{
+    /*
+    [TestClass]
+    class ValidationOfRequestTests
+    {
+        [TestMethod]
+        public void ValidateRequest_PassTheCheck_ReturnTrue()
+        {
+            string stringRequest = "POST/resurs/1";
+            var validation = new ValidationOfRequest();
+
+            var result = validation.ValidateRequest(stringRequest);
+
+            Assert.IsTrue(result);
+        }
+        */
+
+    }
+
